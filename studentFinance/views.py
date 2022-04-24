884a86f32006286812279065cdb5ee884ae9c945
Ankit Goel <59352341+ankitgoel2001@users.noreply.github.com>
2022-04-24 04:45:53 -0700

    everything

--- studentFinance/views.py
+++ studentFinance/views.py
@@ -6,4 +6,28 @@ def get_index(request):
     return render(request, 'index.html')
 
 def display_bank(request):
-    return render(request, 'bank.html')
+	data = {
+		'firstName': request.POST['firstName'],
+		'lastName': request.POST['lastName'],
+        'university': request.POST['university'],
+		'dining':int(request.POST['dining']),
+		'housing':int(request.POST['housing']),
+		'salary':int(request.POST['salary']),
+		'parking':int(request.POST['parking']),
+		'annualTuition':int(request.POST['annualTuition']),
+		'budget':int(request.POST['budget'])
+	}
+	cost = data['dining'] + data['housing'] + data['parking'] + (data['annualTuition'] // 12)
+	amountSpent = cost - data['salary']
+	amountLeft = data['budget'] - amountSpent
+
+	costData = {
+		'restaurants': 0.2 * amountLeft,
+		'groceries': 0.3 * amountLeft,
+		'medicalSupplies': 0.05 * amountLeft,
+		'travel': 0.2 * amountLeft,
+		'others': 0.25 * amountLeft
+	}
+	
+	
+	return render(request, 'bank.html', {'data': data, 'costData': costData})
